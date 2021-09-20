4dbf8697735ab2cdaa2713cec3d416e179036ca2
Filippo Ranza <filipporanza@gmail.com>
2021-09-20 18:06:17 +0200

    Set parametes to correct node

--- run_env.py
+++ run_env.py
@@ -65,14 +65,14 @@ def generate_launch_description():
             package="controller",
             executable="controller",
             name="main_controller",
-            parameters=[
-                {"conveior_speed": 10},
-            ],
         ),
         Node(
             package="conveior_belt",
             executable="conveior_belt",
             name="conveior_belt",
+            parameters=[
+                {"conveior_speed": 10},
+            ],
         ),
     ]
 
